61e270f07621b2585130a0f406d8c05a7add38f9
Duszku <duszku511@gmail.com>
2025-08-08 23:07:36 +0200

    Initial commit
    This is a simple utility that dumps regions of memory-mapped registers
    for two implementations and then compares them register-by-register.
    
    It is a situational, unpolished tool that i hacked quickly to debug my
    own projects. There is likely a million edge cases that are not handled
    and it may require some modification to work for boards that are a
    little more tricky to debug as a remote GDB target.
    
    Signed-off-by: Duszku <duszku511@gmail.com>

---
+++ compare_regs.sh
@@ -0,0 +1,164 @@
+#!/bin/bash
+
+set -euo pipefail
+
+
+################################################################################
+# VARIABLES
+################################################################################
+
+WIDTH=""
+START=""
+TARGET=""
+HAS_DESC=0
+REGISTERS=()
+DUMP1_LOCATION=""
+DUMP2_LOCATION=""
+
+
+################################################################################
+# HELPER FUNCTIONS
+################################################################################
+
+err() {
+	>&2 printf "\033[31mERROR\033[0m\t%s\n" "${@}"
+	exit 1
+}
+
+pause() {
+	read -rn1 -p "Press any key to continue..." discard
+}
+
+it() { printf "\033[3m%s\033[0m" "${@}"; }
+bf() { printf "\033[1m%s\033[0m" "${@}"; }
+
+usage() {
+	cat <<EOF
+$(bf Usage:) ${0} $(it DESCFILE)
+
+Parameters:
+  $(bf DESCFILE)  Register description file
+
+Description:
+
+  Make sure the board invokes a breakpoint instruction at a relevant location.
+  The script will guide you through switching implementations and pressing reset 
+  button.
+EOF
+}
+
+run_gdb() {
+	end=$(( ${START} + ${WIDTH} * ${#REGISTERS[@]} ))
+
+gdb >/dev/null 2>&1 <<EOF
+target ${TARGET}
+continue
+
+append binary memory ${1} ${START} $(printf "0x%x" "${end}")
+exit
+y
+EOF
+}
+
+
+################################################################################
+# ACTIONS
+################################################################################
+
+
+if [ ${#} -lt 1 ]; then
+	usage
+	exit 1
+fi
+
+
+# Locate header boundaries
+
+mapfile -t DELIMITERS < <(grep -n -- --- "${1}" | cut -d: -f1)
+if [ ${#DELIMITERS[@]} -ne 2 ]; then
+	err "File contains too many section delimiters"
+fi
+
+DELIMITERS[0]=$(( ${DELIMITERS[0]} + 1))
+DELIMITERS[1]=$(( ${DELIMITERS[1]} - 1))
+
+
+# Process file header
+
+getval() { echo "${1}" | cut -d: -f2-; }
+while IFS="\n" read -r line; do
+	case "${line}" in
+		DESCRIPTION)
+			HAS_DESC=1
+			;;
+		Width:*)
+			WIDTH=$(getval "${line}")
+			;;
+		Start:*)
+			START=$(getval "${line}")
+			;;
+		Target:*)
+			TARGET=$(getval "${line}")
+			;;
+		*)
+			err "Unrecognized header field"
+	esac
+done < <(head -n "${DELIMITERS[1]}" "${1}" | tail -n +"${DELIMITERS[0]}")
+
+
+if [ "${HAS_DESC}" == "0" ]; then
+	err "File does not not contain a proper header"
+fi
+
+
+# Get register list
+
+LIST_OFFSET=$(( ${DELIMITERS[1]} + 2 ))
+mapfile -t REGISTERS < <(tail -n +"${LIST_OFFSET}" "${1}")
+if [ ${#REGISTERS[@]} -eq 0 ]; then
+	err "No registers listed in file"
+fi
+
+
+# Create temporary files
+
+DUMP1_LOCATION="$(mktemp)"
+DUMP2_LOCATION="$(mktemp)"
+
+# Collect first dump
+
+echo "Please prepare the first implementation"
+pause
+run_gdb "${DUMP1_LOCATION}"
+
+
+# Collect second dump
+
+echo "Please prepare the second implementation"
+pause
+run_gdb "${DUMP2_LOCATION}"
+
+
+# Compare files
+
+LONGEST=0
+for reg in ${REGISTERS[@]}; do
+	if [ ${#reg} -gt ${LONGEST} ]; then
+		LONGEST=${#reg}
+	fi
+done
+
+gethex() { xxd -eg4 -c 4 "${1}" | awk '{print "0x" $2}'; }
+
+echo
+paste <(gethex "${DUMP1_LOCATION}")      \
+      <(gethex "${DUMP2_LOCATION}")      \
+      <(printf "%s\n" "${REGISTERS[@]}") \
+| while IFS=$'\t' read -r val1 val2 name; do
+	if [ "${val1}" != "${val2}" ]; then
+		len=$(( ${LONGEST} - ${#name} + 1 ))
+		printf "Value of ${name} differs:"
+		printf "%${len}s" " " 
+		printf "${val1} vs ${val2}\n"
+	fi
+done
